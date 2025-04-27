6b9be7a9a6fc9c603fd77f539b364ea50678354a
Aakash Basnet <iamaakashbasnet@gmail.com>
2025-04-27 21:07:15 +0545

    chore: change db from sqlite to postgres

--- backend/django/NotesAPI/settings.py
+++ backend/django/NotesAPI/settings.py
@@ -76,9 +76,13 @@ WSGI_APPLICATION = 'NotesAPI.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        'NAME': 'multistack',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
