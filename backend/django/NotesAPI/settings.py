e62e6bd08bd5a57231cbb162bee17fe46a7599ae
Aakash Basnet <iamaakashbasnet@gmail.com>
2025-05-13 13:04:48 +0545

    refactor: rename db name

--- backend/django/NotesAPI/settings.py
+++ backend/django/NotesAPI/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'NotesAPI.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        'NAME': 'multistack',
+        'NAME': 'multistack-auth',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
         'HOST': 'localhost',
